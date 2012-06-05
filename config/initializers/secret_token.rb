e215f3a1936c10e48037825257b198dc7844ced2
singsai <crazyfarm@Nicks-MacBook-Pro.local>
2012-06-05 15:49:31 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pling::Application.config.secret_token = '90f03a302d5f81ca26a891911d48d6043253eec5cc5e18c548d9c8112d2120a78cce33833e61028191de65ee25b5dd611c32c42a7595a465bdb797b02b7757a5'

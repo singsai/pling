e373d2f714c37ea807967cb9fc01dadd64388e56
singsai <crazyfarm@Nicks-MacBook-Pro.local>
2012-06-05 16:18:22 -0600

    Add StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Pling::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/about"
+
+  get "static_pages/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 

1a13ce14190e7cd28ceeb9358b29628594d6c2e6
singsai <crazyfarm@Nicks-MacBook-Pro.local>
2012-06-06 02:08:39 -0600

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,14 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   
   before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
 end
+
+private
+
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
+  end
 # == Schema Information
 #
 # Table name: users
